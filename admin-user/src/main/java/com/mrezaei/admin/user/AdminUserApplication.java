6cd7ab5b701552c1ac68a7421f6773ce7072bf01
m.rezaei <m.rezayi69@gmail.com>
2023-03-03 23:41:12 +0330

    - implement config server - load database and eureka-server address from config server

--- admin-user/src/main/java/com/mrezaei/admin/user/AdminUserApplication.java
+++ admin-user/src/main/java/com/mrezaei/admin/user/AdminUserApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-
 @EnableDiscoveryClient
 @EntityScan("com.mrezaei")
 @ComponentScan("com.mrezaei")
